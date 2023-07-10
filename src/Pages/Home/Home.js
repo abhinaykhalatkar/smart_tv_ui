94eb5a9530e171745d47e625f414166739356475
abhinaykhalatkar <abhinaykhalatkar@gmail.com>
2023-07-10 15:32:29 +0200

    currect project merge

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,28 @@
-import "./Home.scoped.scss"
-function Home() {
+import React from 'react';
+import './Home.scss';
+import prime from '../../assets/prime.png'
+import netflix from '../../assets/netflix.png';
+import disney from '../../assets/d.png';
+import youtube from '../../assets/youtube.png';
+import userProfile from '../../assets/User profile.png';
+import game from '../../assets/game.png';
+import Netflix from '../../Units/Netflix/Netflix';
+import Playstation from '../../Units/Playstation/Playstation';
+
+const Home = () => {
     return (
-      <div>
-        <h1>This is the home page</h1>
-      </div>
+        <div className="home-page">
+            <div className="side-nav" data-aos="fade-right">
+                <Playstation />
+                <Netflix />
+            </div>
+            <main>
+                <header className="h-header" data-aos="fade-down-left">
+                    <img src={userProfile} alt="" />
+                </header>
+            </main>
+        </div>
     );
-  }
-  
-  export default Home;
+};
+
+export default Home;
